ff569063e55bac37a6e4e5026c8a7ac3ecf3ebd9
Cole French <ColeFrench@users.noreply.github.com>
2017-05-17 07:36:20 -0400

    Edit content

---
+++ schoolwork/einstein-atomic-age/assets/js/main.js
@@ -0,0 +1,32 @@
+/*
+var delay = 500;
+
+$(".image").hover(function() {
+    var p = $(this).find("> p");
+    var newHeight = 0;
+
+    p.removeClass("d-none").show();
+    newHeight = $(this).height();
+    p.hide();
+
+    $(this).animate({
+        height: newHeight
+    }, delay);
+
+    p.show();
+}, function() {
+    var p = $(this).find("> p");
+    var newHeight = 0;
+
+    p.hide();
+    newHeight = $(this).height();
+    p.show();
+
+    $(this).animate({
+        height: newHeight
+    }, delay);
+
+    p.hide();
+    $(this).css("height", "");
+});
+*/
