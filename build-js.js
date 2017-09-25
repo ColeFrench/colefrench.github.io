319a5f8b8ee6017fb702582ab8dccef758fd2327
ColeFrench <ColeFrench@users.noreply.github.com>
2017-09-24 22:12:30 -0400

    Update build scripts

--- build-js.js
+++ build-js.js
@@ -1,3 +1,28 @@
 'use strict';
 
-console.log('build-js.js running...');
+const fs = require('fs');
+const glob = require('glob');
+const mkdirp = require('mkdirp');
+const UglifyJS = require('uglify-js');
+
+const outputDir = 'build';
+const globOptions = {
+    ignore: [
+        'node_modules/**/*',
+        'build/**/*'
+    ]
+};
+const files = glob.sync('**/assets/js/*.js', globOptions);
+
+function writeFileSync(path, contents) {
+    const dir = path.substring(0, path.lastIndexOf('/'));
+    mkdirp.sync(dir);
+    fs.writeFileSync(path, contents);
+}
+
+for (var i = 0; i < files.length; ++i) {
+    const file = files[i];
+    const jsIn = fs.readFileSync(file);
+    const jsOut = UglifyJS.minify(jsIn).code;
+    writeFileSync([outputDir, file].join('/'), jsOut);
+}
