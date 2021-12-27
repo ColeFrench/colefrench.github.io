abcfcf0c99cab2de71073504d6f95e7f2112a04e
Cole French <16979554+ColeFrench@users.noreply.github.com>
2021-12-27 13:11:46 -0500

    Toggle hamburger menu on all triggers

--- src/components/Template/Hamburger.js
+++ src/components/Template/Hamburger.js
@@ -24,7 +24,7 @@ const Hamburger = () => {
         </ul>
       </nav>
       <Suspense fallback={<></>}>
-        <Menu right isOpen={open}>
+        <Menu right isOpen={open} onOpen={() => setOpen(true)} onClose={() => setOpen(false)}>
           <ul className="hamburger-ul">
             {routes.map((l) => (
               <li key={l.label}>
