1ad791c5efe4f16a7005b3b7ecd01777dcb7dc88
Cole French <16979554+ColeFrench@users.noreply.github.com>
2021-12-28 14:58:22 -0500

    Introduce myself

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -8,10 +8,6 @@ import Main from '../layouts/Main';
 // uses babel to load contents of file
 const markdown = raw('../data/about.md');
 
-const count = markdown.split(/\s+/)
-  .map((s) => s.replace(/\W/g, ''))
-  .filter((s) => s.length).length;
-
 // Make all hrefs react router links
 const LinkRenderer = ({ ...children }) => <Link {...children} />;
 
@@ -20,8 +16,7 @@ const Index = () => (
     <article className="post markdown" id="index">
       <header>
         <div className="title">
-          <h2 data-testid="heading"><Link to="/">About Me</Link></h2>
-          <p>(in about {count} words)</p>
+          <h2 data-testid="heading"><Link to="/">About</Link></h2>
         </div>
       </header>
       <ReactMarkdown
